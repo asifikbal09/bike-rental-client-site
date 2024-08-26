82876720768d1147223b270aa8ff5ebac6be0a45
asifikbal09 <asif.ikbal662@gmail.com>
2024-08-27 00:43:37 +0600

    [DONE] frist commite.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="font-rajdhani text-xl font-medium">Welcome to our bike rental client site.</h1>
+    </>
+  );
+};
+
+export default App;
